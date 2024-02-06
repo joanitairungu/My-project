20d99364d5187fb75d20ee3ce377ecf72574dbc5
joanitairungu <157994023+joanitairungu@users.noreply.github.com>
2024-02-06 22:24:57 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("My name is Wanjiku!\n");
+    printf("My favorite colour is pink!\n");
+    printf("I am a christian!\n");
+    return 0;
+}
